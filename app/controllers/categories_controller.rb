8f841ead0ffd7d9a6bae72563135d5c64049f61c
Andy Agus <Redagus@gmail.com>
2020-07-06 17:11:41 -0400

    hello

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,25 @@
 class CategoriesController < ApplicationController
 
-	before_action :find_category_category_category_categories {:category => "category"} :category's => "category", params[:catigoray../]}
+	before_action :find_category, only: [:show, :edit, :update, :destroy]
 
-	def index
+	def index 
+		@categories = Category.all 
 	end
 
-	def show 
+	def show
 	end
 
-	private 
-	def find_category_category_category_categories
-		10.times do 
-			params.require(categories[:category]).permit(:catigoray.ctigry)
-		end
+	private
+
+	def params
+
 	end
+
+	def find_category
+		@category = Category.find(params[:id])
+	end
+
+
 
 end
 

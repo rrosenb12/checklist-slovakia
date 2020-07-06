64bf75d27a525d0cea3bb9fa33d778ec41ebc84c
Andy Agus <Redagus@gmail.com>
2020-07-06 17:04:41 -0400

    Category edits

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,19 @@
 class CategoriesController < ApplicationController
+
+	before_action :find_category_category_category_categories {:category => "category"} :category's => "category", params[:catigoray../]}
+
+	def index
+	end
+
+	def show 
+	end
+
+	private 
+	def find_category_category_category_categories
+		10.times do 
+			params.require(categories[:category]).permit(:catigoray.ctigry)
+		end
+	end
+
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.destroy_all
+Timer.destroy_all
+User.destroy_all
+Task.destroy_all
+TaskChecklist.destroy_all
+UserChecklist.destroy_all
+
+puts "creating categories"
+10.times do 
+	Category.create(category_name: Faker::Color.color_name)
+end
+
+puts "creating checklist"
+10.times do 
+	Checklist.create(category_id: Category.all.sample.id, title: Faker::Military.navy_rank)
+end
+
+puts "creating timers"
+10.times do 
+	Timer.create(minutes: rand(1..100))
+end
+
+puts "creating tasks"
+10.times do 
+	Task.create(timer_id: Timer.all.sample.id, category_id: Category.all.sample.id)
+end
+
+puts "creating task-checklists"
+10.times do 
+	TaskChecklist.create(task_id: Task.all.sample.id, checklist_id: Checklist.all.sample.id)
+end
+
+puts "creating users"
+10.times do 
+	User.create(username: Faker::Music::GratefulDead.player, password: Faker::Color.hex_color )
+end
+
+puts "creating user-checklists"
+10.times do
+	UserChecklist.create(user_id: User.all.sample.id, checklist_id: Checklist.all.sample.id)
+end
